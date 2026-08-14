6e92f62e3b304f58229bdeddede76855ce83de8c
aspix <10401349+aspix2k@users.noreply.github.com>
2026-08-14 23:56:15 +0300

    Release 3.6.0
    Assemble the nested Cargo product note and leave Unreleased empty.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "com.aspix2k"
-version = "3.5.0"
+version = "3.6.0"
 
 repositories {
     val mavenCentralMirror = "https://cache-redirector.jetbrains.com/repo1.maven.org/maven2"
