9a5b368349db6d5b2b381c38863d74bd21ad23eb
aspix <10401349+aspix2k@users.noreply.github.com>
2026-08-16 02:42:09 +0300

    release: cut 3.13.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "com.aspix2k"
-version = "3.13.0"
+version = "3.13.1"
 
 repositories {
     val mavenCentralMirror = "https://cache-redirector.jetbrains.com/repo1.maven.org/maven2"
