322051dbe8451a9ee0e69f40744096d8710c38f6
Aleksei Larionov <10401349+aspix2k@users.noreply.github.com>
2026-08-16 13:05:53 +0300

    release: cut 3.13.5 (#352)

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "com.aspix2k"
-version = "3.13.4"
+version = "3.13.5"
 
 repositories {
     val mavenCentralMirror = "https://cache-redirector.jetbrains.com/repo1.maven.org/maven2"
